56012137b86cb8f931156688befdb6e95b7d360f
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2024-12-11 08:12:19 +0000

    left-pad solution numbers with 0 so they all line up nicely

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Getting some TS practice, be gentle.
 
-To run, add input (e.g. `1-1.txt`) to `inputs/` then do:
+To run, add input (e.g. `01-1.txt` - with leading 0 up to 2 digits) to `inputs/` then do:
 
 ```
 npm run solution -- 1-1
--- runner.ts
+++ runner.ts
@@ -4,7 +4,13 @@ type Solution = {
   default: (input: string) => any;
 }
 
-const day: string = process.argv[2];
+let day: string = process.argv[2];
+
+// 9-1 -> 09-1
+if (day.length === 3) {
+  day = '0' + day;
+}
+
 const input: string = readFileSync(`./inputs/${day}.txt`, { encoding: 'utf8' });
 const solution: Solution = await import(`./solutions/${day}.ts`);
 
